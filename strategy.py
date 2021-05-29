ac7df916b11c02b271f296f2b5024c0d573c27ed
Endrik Tombak <littleendu@gmail.com>
2021-05-29 17:11:06 +0300

    Reduce main_loop complexity

--- strategy.py
+++ strategy.py
@@ -36,14 +36,10 @@ def get_target_pixels(target_filename, top_down=True):
             if get_pixel_diff(canvas_pixel[:3], target_pixel[:3]) > current_pixel_leniency:
                 rv.append((x, y, '%02x%02x%02x' % target_pixel[:3]))
 
-    if top_down:
-        for xx in range(ww):
-            for yy in range(hh):
-                compare_pixel(xx, yy)
-    else:
-        for yy in range(hh):
-            for xx in range(ww):
-                compare_pixel(xx, yy)
+    first, second = (ww, hh) if top_down else (hh, ww)
+    for f in range(first):
+        for s in range(second):
+            compare_pixel(f, s) if top_down else compare_pixel(s, f)
 
     target_img.close()
     return rv, total
@@ -58,32 +54,31 @@ def main_loop():
         # noinspection PyBroadException
         try:
             is_100 = True
-            for td in (True, False):
-                for rev in (-1, 1):
-                    api.wait_for_set_pixel()
-                    for file_name in os.listdir('maintain'):
-                        target_pixels, total = get_target_pixels(f'maintain/{file_name}', td)
-                        left = len(target_pixels)
-                        done = total - left
-                        percent = done / total
-                        if target_pixels:
-                            is_100 = False
-                            print_100 = False
-                            logger.info(f"Working on {file_name} {int(percent * 100)}% "
-                                        f"{done}/{left}/{total} d~{current_pixel_leniency}")
-                            candidate = target_pixels[int(random.random() ** 0.1 * len(target_pixels) * rev)]
-                            api.set_pixel(*candidate)
-                            target_pixels.remove(candidate)
-                            break  # because we worked on this file, we want to come back to in the next round
-                if is_100:
-                    current_pixel_leniency = max(0, current_pixel_leniency - 1)
-                    if not print_100:
-                        logger.info("All images 100% done")
-                        print_100 = True
-                    time.sleep(5)
-                    api.wait_for_get_pixels()
-                else:
-                    current_pixel_leniency = min(config.IMAGE_MAX_LENIENCY, current_pixel_leniency + 1)
+            for rev, td in [(-1, True), (-1, False), (1, True), (1, False)]:
+                api.wait_for_set_pixel()
+                for file_name in os.listdir('maintain'):
+                    target_pixels, total = get_target_pixels(f'maintain/{file_name}', td)
+                    left = len(target_pixels)
+                    done = total - left
+                    percent = done / total
+                    if target_pixels:
+                        is_100 = False
+                        print_100 = False
+                        logger.info(f"Working on {file_name} {int(percent * 100)}% "
+                                    f"{done}/{left}/{total} d~{current_pixel_leniency}")
+                        candidate = target_pixels[int(random.random() ** 0.1 * len(target_pixels) * rev)]
+                        api.set_pixel(*candidate)
+                        target_pixels.remove(candidate)
+                        break  # because we worked on this file, we want to come back to in the next round
+            if is_100:
+                current_pixel_leniency = max(0, current_pixel_leniency - 1)
+                if not print_100:
+                    logger.info("All images 100% done")
+                    print_100 = True
+                time.sleep(5)
+                api.wait_for_get_pixels()
+            else:
+                current_pixel_leniency = min(config.IMAGE_MAX_LENIENCY, current_pixel_leniency + 1)
         except Exception:
             logger.exception('Exception in main loop')
             logger.info(f"Main loop is sleeping for {utils.sleep_until(on_exception_time, True)}")
